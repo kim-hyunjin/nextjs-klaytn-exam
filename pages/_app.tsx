e80d7fe66d1169b22bb3f9a5bc6ab2ae390e03bf
kim-hyunjin <hyunjin1612@gmail.com>
2022-03-28 19:45:50 +0900

    metamask + web-react

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ExternalProvider, Web3Provider } from "@ethersproject/providers";
+import { Web3ReactProvider } from "@web3-react/core";
+import { createContext } from "react";
+import { RootStore } from "../store/RootStore";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function getLibrary(provider: ExternalProvider) {
+  const library = new Web3Provider(provider);
+  library.pollingInterval = 12000;
+  return library;
 }
 
-export default MyApp
+const rootStore = new RootStore();
+export const WalletLibraryContext = createContext<RootStore>(rootStore);
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <WalletLibraryContext.Provider value={rootStore}>
+        <Component {...pageProps} />;
+      </WalletLibraryContext.Provider>
+    </Web3ReactProvider>
+  );
+}
+
+export default MyApp;
